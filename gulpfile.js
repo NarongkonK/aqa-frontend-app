ed8b53cac005cd1f2ece30951c1cee0f3f8d8434
Pongpanot Tassaneeyachumphalee <pongpanotice@hotmail.com>
2018-02-06 01:10:11 +0700

    breadcrumb

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,12 @@ const async = require('async')
 const fs = require('fs')
 const cheerio = require('cheerio')
 
-gulp.task('route', function(){
+gulp.task('route', function () {
 
     glob("./src/routes/**/route-*.html", null, function (er, files) {
 
-        const addAttribute = (name,value) => {
-            if(value){
+        const addAttribute = (name, value) => {
+            if (value) {
                 return `${name}="${value}"`
             }
             return ""
@@ -19,18 +19,19 @@ gulp.task('route', function(){
 
         let el = ''
         let im = ''
+        // let root = []
 
         async.each(files, (file, callback) => {
 
             console.log('Processing file : ' + file)
 
             let splitSlash = file.split('/')
-            let fileName = splitSlash[splitSlash.length-1]
+            let fileName = splitSlash[splitSlash.length - 1]
             let elementName = fileName.split('.')[0]
-            let pathRoute = '/' + elementName.split('route-')[1].replace('-','/')
+            let pathRoute = '/' + elementName.split('route-')[1].replace('-', '/')
             let pathFile = file.split('./src/')[1]
 
-            
+
 
             fs.readFile(file, "utf8", (err, data) => {
                 if (err) {
@@ -39,26 +40,37 @@ gulp.task('route', function(){
                 const $ = cheerio.load(data)
 
                 pathRoute = $('dom-module').attr('path') || pathRoute
-                let topic = $('dom-module').attr('topic') 
-             
-                
+                let topic = $('dom-module').attr('topic')
+
+                const rule = function () {
+                    if ($('dom-module').attr('rule') != undefined) {
+                        return 'rule'
+                    } else {
+                        return ''
+                    }
+                }
 
                 im = im + `<link rel="lazy-import" href="${pathFile}">\n`
-                el = el + `<${elementName} ${addAttribute('topic',topic)} path-file="${pathFile}" path="${pathRoute}"></${elementName}>\n`
+                el = el + `<${elementName} ${addAttribute('topic', topic)} path-file="${pathFile}" path="${pathRoute}" ${rule()}></${elementName}>\n`
+
+                // root.push({
+                //     name: elementName,
+                //     topic
+                // })
 
                 callback();
             });
 
-        }, function(err) {
-            if( err ) {
-                console.log('error: '+ err);
+        }, function (err) {
+            if (err) {
+                console.log('error: ' + err);
             } else {
                 gulp.src(['./src/nylon-init-template.html'])
-                .pipe(replace('<!-- [[gen_import]] -->', im))
-                .pipe(replace('<!-- [[gen_route]] -->', el))
-                .pipe(rename('nylon-init.html'))
-                .pipe(gulp.dest('./src/'))
-                
+                    .pipe(replace('<!-- [[gen_import]] -->', im))
+                    .pipe(replace('<!-- [[gen_route]] -->', el))
+                    .pipe(rename('nylon-init.html'))
+                    .pipe(gulp.dest('./src/'))
+
                 //console.log(el)
                 console.log('**************')
                 console.log('successfully')
@@ -69,7 +81,7 @@ gulp.task('route', function(){
 
 
 
-        
+
 
         // gulp.src(['./src/nylon-init-template.html'])
         // .pipe(replace('<!-- [[gen_import]] -->', im))
@@ -79,107 +91,211 @@ gulp.task('route', function(){
 
         //console.log(el)
 
-        
+
     })
-    
-  });
+
+});
 
 
 
-gulp.task('route_bak2', function(){
+gulp.task('route2', function () {
 
     glob("./src/routes/**/route-*.html", null, function (er, files) {
-  
 
-        let el = ''
-        let im = ''
+        let elementList = []
 
-        files.map((file)=>{
-            
-            var splitSlash = file.split('/')
-            var fileName = splitSlash[splitSlash.length-1]
-            var elementName = fileName.split('.')[0]
-            var pathRoute = '/' + elementName.split('route-')[1].replace('-','/')
-            var pathFile = file.split('./src/')[1]
+        async.each(files, (file, callback) => {
 
-            im = im + `<link rel="lazy-import" href="${pathFile}">\n`
-            el = el + `<${elementName} path-file="${pathFile}" path="${pathRoute}"></${elementName}>\n`
+            console.log('Processing file : ' + file)
 
-        })
-        
-
-        gulp.src(['./src/nylon-init-template.html'])
-        .pipe(replace('<!-- [[gen_import]] -->', im))
-        .pipe(replace('<!-- [[gen_route]] -->', el))
-        .pipe(rename('nylon-init.html'))
-        .pipe(gulp.dest('./src/'))
-
-        console.log(el)
-
-        
-    })
-    
-  });
+            let splitSlash = file.split('/')
+            let fileName = splitSlash[splitSlash.length - 1]
+            let elementName = fileName.split('.')[0]
+            let pathRoute = '/' + elementName.split('route-')[1].replace('-', '/')
+            let pathFile = file.split('./src/')[1]
 
 
 
+            fs.readFile(file, "utf8", (err, data) => {
+                if (err) {
+                    callback(err)
+                }
+                const $ = cheerio.load(data)
+                let topic = $('dom-module').attr('topic')
 
+                elementList.push({
+                    elementName,
+                    pathFile,
+                    pathRoute: $('dom-module').attr('path') || pathRoute,
+                    rule: $('dom-module').attr('rule') != undefined,
+                    topic: $('dom-module').attr('topic'),
+                    parentName: $('dom-module').attr('parent')
+                })
 
-gulp.task('route_bak', function(){
+                callback();
+            });
 
-    glob("./src/route/**/*-route.html", null, function (er, files) {
-        const snakeToCamel = function(s){
-            return s.replace(/(\-\w)/g, function(m){return m[1].toUpperCase();});
-        }
+        }, function (err) {
+            if (err) {
+                console.log('error: ' + err);
+            } else {
+                //console.log(elementList)
 
-        var pageList = files.map(function(file){
-            var fileSplit1 = file.split('/')
-            var fileName = fileSplit1[fileSplit1.length-1]
+                const addAttribute = (name, value) => {
+                    if(value != undefined){
 
-            var elementName = fileName.split('-route.html')[0]
+                        if(typeof value == "boolean"){
+                             if(value){
+                                 return name
+                             }else{
+                                 return ''
+                             }
+                        }
+                        return `${name}="${value}"`
+                    }
+                    return ''
+                }
 
-            var pathSplit1 = file.split('/'+fileName)
-            var pathSplit2 = pathSplit1[0]
-            var pathSplit3 = pathSplit2.split('./src/route/')[1]
-            var pathSplit4 = elementName.split('page-')[1]
-            
-            
-            var pathName = '';
-            if(pathSplit4 == pathSplit3.replace('/','-')){
-                pathName = pathSplit3
-            }else{
-                pathName = pathSplit3+'/'+pathSplit4.split(pathSplit3.replace('/','-'))[1].replace('-','')
+                const navigation = (elementNameSource) => {
+                    const navigationIn = (elementName) => {
+                        elementNameCut = elementName.split('>')[0]
+                        let getPage = elementList.filter((row) => {
+                            return row.elementName == elementNameCut
+                        })
+
+                        if (getPage.length != 0) {
+                            if (getPage[0].parentName) {
+                                return navigationIn(getPage[0].parentName + '>' + elementName)
+                            }
+                        }
+                
+                        return elementName
+                    }
+
+                    return navigationIn(elementNameSource).split('>')
+                }
+                
+                let im = ''
+                let el = ''
+                let nav = {}
+
+                elementList.map((row)=>{
+                    im = im + `<link rel="lazy-import" href="${row.pathFile}">\n`
+                    el = el + `<${row.elementName} ${addAttribute('topic', row.topic)} path-file="${row.pathFile}" path="${row.pathRoute}" ${addAttribute('rule',row.rule)} ${addAttribute('parent',row.parentName)}></${row.elementName}>\n`
+                    nav[row.elementName] = navigation(row.elementName).map((row)=>{
+
+                        eln = elementList.filter((row2)=>{
+                            return row2.elementName == row
+                        })[0]
+
+                        return {
+                            elementName: row,
+                            url: eln.pathRoute,
+                            label: eln.topic
+                        }
+                    })
+                })
+
+                //console.log(nav)
+
+                gulp.src(['./src/nylon-init-template.html'])
+                    .pipe(replace('<!-- [[gen_import]] -->', im))
+                    .pipe(replace('<!-- [[gen_route]] -->', el))
+                    .pipe(replace('gen_navigation', JSON.stringify(nav)))
+                    .pipe(rename('nylon-init.html'))
+                    .pipe(gulp.dest('./src/'))
+
+                console.log('**************')
+                console.log('successfully')
             }
-            pathName = '/'+pathName
+        });
 
-            return {
-                file: fileName,
-                el: elementName,
-                path: pathName,
-                pathFile: pathSplit3
-            }
-        })
 
-        var el = ''
-        var im = ''
 
-        pageList.map((row)=>{
-            var pathFile = `./route/${row.pathFile}/${row.el}-route.html`
-            im = im + `<link rel="lazy-import" href="${pathFile}">\n`
-            el = el + `<${row.el} path-file="${pathFile}" path="${row.path}"></${row.el}>\n`
-        })
+
+
+
+
+        // gulp.src(['./src/nylon-init-template.html'])
+        // .pipe(replace('<!-- [[gen_import]] -->', im))
+        // .pipe(replace('<!-- [[gen_route]] -->', el))
+        // .pipe(rename('nylon-init.html'))
+        // .pipe(gulp.dest('./src/'))
 
         //console.log(el)
 
-        gulp.src(['./src/nylon-init-template.html'])
-        .pipe(replace('<!-- [[gen_import]] -->', im))
-        .pipe(replace('<!-- [[gen_route]] -->', el))
-        .pipe(rename('nylon-init.html'))
-        .pipe(gulp.dest('./src/'))
 
-        console.log(el)
-
-        
     })
-    
-  });
+
+});
+
+
+
+
+
+gulp.task('test', function () {
+    let list = [
+        { name: 'A' },
+        { name: 'B', parent: 'A' },
+        { name: 'C', parent: 'B' },
+        { name: 'D', parent: 'C' },
+        { name: 'E', parent: 'D' },
+        { name: 'F', parent: 'E' }
+    ]
+
+    let data = list.filter((row) => {
+        return row.parent == undefined
+    })
+
+    const getRoot = function (list, data) {
+        let root = data.map(function (row, i) {
+            let child = list.filter(function (row2) {
+                return row2.parent == row.name
+            })
+
+            if (child.length > 0) {
+                row.child = getRoot(list, child)
+            }
+            return row
+        })
+
+        return root
+    }
+
+
+    console.log(getRoot(list, data)[0].child[0])
+
+
+});
+
+
+
+gulp.task('test2', function () {
+    let list = [
+        { name: 'A' },
+        { name: 'B', parent: 'A' },
+        { name: 'C', parent: 'B' },
+        { name: 'D', parent: 'C' },
+        { name: 'E', parent: 'D' },
+        { name: 'F', parent: 'E' }
+    ]
+
+    const navigate = (page) => {
+        pageCut = page.split('>')[0]
+        let getPage = list.filter((row) => {
+            return row.name == pageCut
+        })
+
+        if (getPage.length != 0) {
+            if (getPage[0].parent) {
+                return navigate(getPage[0].parent + '>' + page)
+            }
+        }
+
+        return page
+    }
+
+    console.log(navigate('D').split('>'))
+
+
+});
