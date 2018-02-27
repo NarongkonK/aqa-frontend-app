2f43f4d022ee5c01b1c913a8f962cec4f79e1cbc
พงศ์ปณต ทัศนียาชุมพาลี <Namkhaeng@Macintosh.local>
2018-02-27 15:23:31 +0700

    update build

--- gulpfile.js
+++ gulpfile.js
@@ -9,39 +9,41 @@ const gulpCopy = require('gulp-copy');
 const del = require('del');
 const exec = require('child_process').exec;
 
+let arrPath = [];
+
 gulp.task('route', function () {
 
     return Promise.all([
-        new Promise(function(resolve, reject) {
-            glob("./src/frontend/routes/**/route-*.html", null, function (er, files) {
-
+        new Promise((resolve, reject) => {
+            glob("./src/frontend/routes/**/route-*.html", null, (er, files) => {
+                arrPath = arrPath.concat(files)
                 let elementList = []
-        
+
                 async.each(files, (file, callback) => {
-        
+
                     console.log('Processing file : ' + file)
-        
+
                     let splitSlash = file.split('/')
                     let fileName = splitSlash[splitSlash.length - 1]
                     let elementName = fileName.split('.')[0]
                     let pathRoute = '/' + elementName.split('route-')[1].replace('-', '/')
                     let pathFile = file.split('./src/')[1]
-        
-        
-        
+
+
+
                     fs.readFile(file, "utf8", (err, data) => {
                         if (err) {
                             callback(err)
                         }
                         const $ = cheerio.load(data)
                         let domModule = $('dom-module')
-                        if($('dom-module').attr('id') == undefined){
+                        if ($('dom-module').attr('id') == undefined) {
                             domModule = $('script')
                         }
-                        
-        
+
+
                         let topic = domModule.attr('topic')
-        
+
                         elementList.push({
                             elementName,
                             pathFile,
@@ -50,64 +52,64 @@ gulp.task('route', function () {
                             topic: domModule.attr('topic'),
                             parentName: domModule.attr('parent')
                         })
-        
+
                         callback();
                     });
-        
+
                 }, function (err) {
                     if (err) {
                         console.log('error: ' + err);
                     } else {
                         //console.log(elementList)
-        
+
                         const addAttribute = (name, value) => {
-                            if(value != undefined){
-        
-                                if(typeof value == "boolean"){
-                                     if(value){
-                                         return name
-                                     }else{
-                                         return ''
-                                     }
+                            if (value != undefined) {
+
+                                if (typeof value == "boolean") {
+                                    if (value) {
+                                        return name
+                                    } else {
+                                        return ''
+                                    }
                                 }
                                 return `${name}="${value}"`
                             }
                             return ''
                         }
-        
+
                         const navigation = (elementNameSource) => {
                             const navigationIn = (elementName) => {
                                 let elementNameCut = elementName.split('>')[0]
                                 let getPage = elementList.filter((row) => {
                                     return row.elementName == elementNameCut
                                 })
-        
+
                                 if (getPage.length != 0) {
                                     if (getPage[0].parentName) {
                                         return navigationIn(getPage[0].parentName + '>' + elementName)
                                     }
                                 }
-                        
+
                                 return elementName
                             }
-        
+
                             return navigationIn(elementNameSource).split('>')
                         }
-                        
+
                         let im = ''
                         let el = ''
                         let nav = {}
-        
-                        elementList.map((row)=>{
+
+                        elementList.map((row) => {
 
                             im = im + `<link rel="lazy-import" href="${row.pathFile}">\n`
-                            el = el + `<${row.elementName} ${addAttribute('topic', row.topic)} path-file="${row.pathFile}" path="${row.pathRoute}" ${addAttribute('rule',row.rule)} ${addAttribute('parent',row.parentName)}></${row.elementName}>\n`
-                            nav[row.elementName] = navigation(row.elementName).map((row)=>{
-        
-                                let eln = elementList.filter((row2)=>{
+                            el = el + `<${row.elementName} ${addAttribute('topic', row.topic)} path-file="${row.pathFile}" path="${row.pathRoute}" ${addAttribute('rule', row.rule)} ${addAttribute('parent', row.parentName)}></${row.elementName}>\n`
+                            nav[row.elementName] = navigation(row.elementName).map((row) => {
+
+                                let eln = elementList.filter((row2) => {
                                     return row2.elementName == row
                                 })[0]
-        
+
                                 return {
                                     elementName: row,
                                     url: eln.pathRoute,
@@ -115,8 +117,8 @@ gulp.task('route', function () {
                                 }
                             })
                         })
-        
-        
+
+
                         gulp.src(['./src/nylon-init-template.html'])
                             .pipe(replace('<!-- [[gen_import]] -->', im))
                             .pipe(replace('<!-- [[gen_route]] -->', el))
@@ -124,54 +126,54 @@ gulp.task('route', function () {
                             .pipe(rename('nylon-init.html'))
                             .pipe(gulp.dest('./src/'))
                             .on('end', resolve)
-                            gulp.start('dialog');
-                        
+                        gulp.start('dialog');
+
                     }
                 });
-        
-        
+
+
             })
         })
     ])
 
-    
+
 
 });
 
 
 
-gulp.task('dialog', function () {
+gulp.task('dialog', () => {
     return Promise.all([
-        new Promise(function(resolve, reject) {
-            glob("./src/frontend/routes/**/dialog-*.html", null, function (er, files) {
-
+        new Promise((resolve, reject) => {
+            glob("./src/frontend/routes/**/dialog-*.html", null, (er, files) => {
+                arrPath = arrPath.concat(files)
                 let elementList = []
-        
+
                 async.each(files, (file, callback) => {
-        
+
                     console.log('Processing file : ' + file)
-        
+
                     let splitSlash = file.split('/')
                     let fileName = splitSlash[splitSlash.length - 1]
                     let elementName = fileName.split('.')[0]
                     let pathRoute = '/' + elementName.split('dialog-')[1].replace('-', '/')
                     let pathFile = file.split('./src/')[1]
-        
-        
-        
+
+
+
                     fs.readFile(file, "utf8", (err, data) => {
                         if (err) {
                             callback(err)
                         }
                         const $ = cheerio.load(data)
                         let domModule = $('dom-module')
-                        if($('dom-module').attr('id') == undefined){
+                        if ($('dom-module').attr('id') == undefined) {
                             domModule = $('script')
                         }
-                        
-        
+
+
                         let topic = domModule.attr('topic')
-        
+
                         elementList.push({
                             elementName,
                             pathFile,
@@ -179,77 +181,105 @@ gulp.task('dialog', function () {
                             rule: domModule.attr('rule') != undefined,
                             topic: domModule.attr('topic')
                         })
-        
+
                         callback();
                     });
-        
+
                 }, function (err) {
                     if (err) {
                         console.log('error: ' + err);
                     } else {
                         //console.log(elementList)
-        
+
                         const addAttribute = (name, value) => {
-                            if(value != undefined){
-        
-                                if(typeof value == "boolean"){
-                                     if(value){
-                                         return name
-                                     }else{
-                                         return ''
-                                     }
+                            if (value != undefined) {
+
+                                if (typeof value == "boolean") {
+                                    if (value) {
+                                        return name
+                                    } else {
+                                        return ''
+                                    }
                                 }
                                 return `${name}="${value}"`
                             }
                             return ''
                         }
-        
+
                         const navigation = (elementNameSource) => {
                             const navigationIn = (elementName) => {
                                 let elementNameCut = elementName.split('>')[0]
                                 let getPage = elementList.filter((row) => {
                                     return row.elementName == elementNameCut
                                 })
-        
+
                                 if (getPage.length != 0) {
                                     if (getPage[0].parentName) {
                                         return navigationIn(getPage[0].parentName + '>' + elementName)
                                     }
                                 }
-                        
+
                                 return elementName
                             }
-        
+
                             return navigationIn(elementNameSource).split('>')
                         }
-                        
+
                         let im = ''
                         let el = ''
-        
-                        elementList.map((row)=>{
+
+                        elementList.map((row) => {
                             im = im + `<link rel="lazy-import" href="${row.pathFile}">\n`
                             el = el + `<${row.elementName} ${addAttribute('label', row.topic)} path-file="${row.pathFile}" path="${row.pathRoute}" lazy></${row.elementName}>\n`
-                           
+
                         })
-        
-        
+
+
                         gulp.src(['./src/nylon-init.html'])
                             .pipe(replace('<!-- [[gen_import_dialog]] -->', im))
                             .pipe(replace('<!-- [[gen_dialog]] -->', el))
                             .pipe(gulp.dest('./src/'))
                             .on('end', resolve)
+                        gulp.start('polymer');
                     }
                 });
-        
-        
+
+
             })
         })
     ])
 
-    
+
 
 });
 
+gulp.task('polymer', function (done) {
+
+    const comArr = (arr)=>{
+        let txt = ""
+        arr.map((row,i)=>{
+            txt = txt+`\t\t"${row}"`
+            if(i<arr.length-1){
+                txt = txt+`,\n`
+            }
+        })
+        return txt
+    }
+    // console.log(arrPath)
+
+    gulp.src('./polymer-template.json')
+        .pipe(replace(`"fragments": [],`, `"fragments": [\n${comArr(arrPath)}\n\t],`))
+        .pipe(rename('polymer.json'))
+        .pipe(gulp.dest('./'))
+
+        .on('end', done)
+
+
+    // glob("./src/frontend/routes/**/dialog-*.html", null, function (er, files) {
+    //     console.log(files)
+    //     done()
+    // })
+})
 
 
 
@@ -260,8 +290,9 @@ gulp.task('dialog', function () {
 
 
 
-const myRename = function(source,destination){
-    return new Promise(function(resolve, reject) {
+
+const myRename = function (source, destination) {
+    return new Promise(function (resolve, reject) {
         fs.rename(source, destination, function (err) {
             if (err) {
                 throw err;
@@ -314,7 +345,7 @@ gulp.task('build:undo_rename', function () {
 
 //     return new Promise(function(resolve, reject) {
 
-       
+
 //         async.series({
 //             renameSym: function(callback) {
 //                 fs.rename('src/frontend', 'src/_frontend', function (err) {
@@ -335,7 +366,7 @@ gulp.task('build:undo_rename', function () {
 //             },
 //             execTest: function(callback){
 //                 exec('polymer build', function (err, stdout, stderr) {
-                 
+
 //                 });
 //             } 
 
